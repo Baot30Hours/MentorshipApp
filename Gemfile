36de30d0483d842fff95e20a63cb960a844a49b5
adipie <adipiek@gmail.com>
2018-05-19 15:34:10 +0300

    add navigaion bar

--- Gemfile
+++ Gemfile
@@ -34,7 +34,10 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'jquery-rails'
+
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
+gem 'autoprefixer-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420143831) do
+ActiveRecord::Schema.define(version: 20180423104533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "connections", force: :cascade do |t|
     t.bigint "Mentee_id"
@@ -33,7 +26,7 @@ ActiveRecord::Schema.define(version: 20180420143831) do
 
   create_table "events", force: :cascade do |t|
     t.bigint "Connection_id"
-    t.string "type"
+    t.string "event_type"
     t.string "method"
     t.datetime "date"
     t.text "notes"
