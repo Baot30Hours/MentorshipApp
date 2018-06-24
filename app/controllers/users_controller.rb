4c409754b03e8edd608281998cb1315d7d3a3954
Gilad Avidan <giladvdn@gmail.com>
2018-06-24 19:37:36 +0100

    Move login/logout to Users controller, remove Sessions controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,38 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def login
+    # Render the login page
+  end
+
+  def do_login
+    # Handle POSTs and log the user in
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      logger.debug "user authenticated!"
+
+      log_in user
+      remember user
+
+      redirect_to :controller => 'welcome', :action => 'index'
+
+    else
+      flash[:danger] = "Invalid email/password combination"
+      logger.debug "Failed to log in"
+
+      redirect_to :action => 'login'
+    end
+  end
+
+  def logout
+    # Logs the user out
+    log_out
+    cookies.delete :user_id
+    cookies.delete :_mentorship_app_session
+    render 'welcome/index'
+  end 
+
   def show
     @user = User.find(params[:id])
   end
