10ee5a332f6b54efb0e1f234b6dfad310cd1b587
adipie <adipiek@gmail.com>
2018-07-03 12:54:45 +0300

    add error messages to mentor form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180618200959) do
+ActiveRecord::Schema.define(version: 20180701113555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20180618200959) do
     t.text "notes"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "company"
   end
 
   create_table "mentors", force: :cascade do |t|
@@ -58,6 +59,7 @@ ActiveRecord::Schema.define(version: 20180618200959) do
     t.integer "mentee_slots"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "company"
   end
 
   create_table "users", force: :cascade do |t|
