238405214be3da4cca4b84b0f0cb0a33ac17a350
adipie <adipiek@gmail.com>
2018-07-09 18:58:39 +0300

    redesign add event form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -69,6 +69,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:timestamp, :event_date, :event_type, :meeting_method, :notes, :connection_id)
+      params.require(:event).permit(:timestamp, :event_date, :event_type, :meeting_method, :notes, :connection_id, :note)
     end
 end
