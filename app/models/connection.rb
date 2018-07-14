1ee6b24f370fdd84bce48b287d45c8ce4c067273
adipie <adipiek@gmail.com>
2018-07-14 11:20:01 +0300

    add display name to connection

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -3,7 +3,7 @@ class Connection < ApplicationRecord
   belongs_to :mentor
   has_many :events
   enum status: [:active, :closed]
-
+  
   def last_updated
   	if (self.events.empty?)
   		self.updated_at
@@ -11,4 +11,9 @@ class Connection < ApplicationRecord
   		return self.events.sort_by(&:created_at).first.updated_at
    end
   end
+
+  def display_name
+    self.mentor.name + " <> " + self.mentee.name
+  end
+
 end
