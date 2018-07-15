48f26283e6eb39bb7b97e00d649d869749d1e0e7
Iris <iris.ronen2906@gmail.com>
2018-07-15 21:09:01 +0300

    Update connection.rb

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -3,12 +3,20 @@ class Connection < ApplicationRecord
   belongs_to :mentor
   has_many :events
   enum status: [:active, :closed]
-
+  validates :mentor, uniqueness: { scope: :mentee, message: ": connection already exists" }
+  validates :mentee, uniqueness: { scope: :mentor, message: ": connection already exists" }
+  validates_associated :mentor, {message: ": mentor has no available slots" }
+  
   def last_updated
   	if (self.events.empty?)
   		self.updated_at
   	else
   		return self.events.sort_by(&:created_at).first.updated_at
-   end
+    end
   end
+
+  def display_name
+    self.mentor.name + " <> " + self.mentee.name
+  end
+
 end
